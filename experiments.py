4a30122477893ff48446ae55ca33c68347db70e6
woai <dingwenbo@gmail.com>
2021-05-08 21:10:24 +0800

    add experiment 1

---
+++ experiments.py
@@ -0,0 +1,56 @@
+import numpy as np
+from optimizers.odh import ODH
+
+def exp0():
+    '''
+    A very simple problem for debug purpose.
+    '''
+    A = np.eye(1)
+    b = np.eye(1)
+    x0 = np.zeros(1)
+    def objFun(x):
+        return 1/2*x*x + x
+
+    def gradFun(x):
+        return x+1
+
+    odhoptim = ODH(x0, objFun, gradFun, A, verbose=True)
+    odhoptim.optimize()
+
+def exp1():
+    ## Experiment 1, Example 2
+    n = 10
+    lambda_min, lambda_max = 1, 1e3
+    u_i = np.append(0.2*np.random.rand(int(np.floor(n/2))), np.ones(int(np.ceil(n/2)))-0.2*np.random.rand(int(np.ceil(n/2))))
+    lambdas = lambda_min + (lambda_max - lambda_min)*u_i
+    A = np.diag(lambdas)
+    x0 = np.zeros(n)
+    b = A*np.random.rand(n)
+
+    def objFun(x):
+        return 1/2 * x.transpose * A * x - x.transpose * b
+
+    def gradFun(x):
+        return A * x - b
+
+    odhoptim = ODH(x0, objFun, gradFun, A, maxIter=5, verbose=True)
+    odhoptim.optimize()
+
+if __name__=='__main__':
+    exp1()
+    ## initialize
+    # n = 5
+    # # x0 = 10*np.random.rand(n).reshape(n,1)
+    # x0 = 2*np.array([1.3, 0.7, 0.8, 1.9, 1.2])
+    # objFun = rosen
+    # gradFun = rosen_der
+    # A = np.eye(n)
+    # odhoptim = ODH(x0, objFun, gradFun, A, verbose=True)
+    # odhoptim.optimize()
+
+    # # compare with scipy.minimize
+    # print('=================nelder-mead================')
+    # res = minimize(rosen, x0, method='nelder-mead',
+    #            options={'xatol': 1e-8, 'disp': True})
+    
+    # print(res.x)
--- optimizers/odh.py
+++ optimizers/odh.py
@@ -10,7 +10,7 @@ class ODH:
                  strategy='ODH1',
                  theta=0.01,
                  kappa=0.1,
-                 maxIter=1e5,
+                 maxIter=1e2,
                  tolerance=1e-6,
                  verbose=False):
         """
@@ -25,7 +25,8 @@ class ODH:
         gradx0 = gradFun(x0)
         self.objFun = objFun
         self.gradFun = gradFun
-        self.x = [x0, self.backtracking(x0, gradx0)]
+        # self.x = [x0, self.backtracking(x0, gradx0)]
+        self.x = [x0, x0+np.random.rand(len(x0))]
         self.alpha = []
         self.g = [gradx0, gradFun(self.x[-1])]
         self.gk_norm = []
@@ -37,6 +38,8 @@ class ODH:
         self.maxIter = maxIter
         self.tolerance = tolerance
         self.verbose = verbose
+        if self.verbose:
+            print("Initial values: ", self.x, self.g)
 
     
     def backtracking(self, x0, g0):
@@ -60,8 +63,8 @@ class ODH:
 
             # update
             xk = self.x[-1] - 1./alphak*self.g[-1]
-            # gk = gk - 1./alphak*A*gk
-            gk = gradFun(xk)
+            gk = self.g[-1] - 1./alphak*self.A*self.g[-1]
+            # gk = self.gradFun(xk)
             self.iter+=1
 
             # recording
@@ -71,23 +74,23 @@ class ODH:
             self.gk_norm.append(gk_norm)
 
             if self.verbose:
-                if self.iter%100==0:
-                    print("Iter:", self.iter, "alphak:", alphak, "the norm of gradient:", gk_norm)
+                if self.iter%1==0:
+                    print("Iter:", self.iter, "alphak:", alphak, "gk_norm:", gk_norm, 'gk', gk,  "x:", xk)
                     # break
 
     def calcAlpha(self, strategy):
         assert strategy in ['ODH1', 'ODH2', 'adaptive1', 'adaptive2']
         # y[k] = g[k] - g[k-1]
-        yk_prev = self.g[-1] - self.g[-2]
+        yk_old = self.g[-1] - self.g[-2]
         # s[k-1] = x[k] - x[k-1]
-        sk_prev = self.x[-1] - self.x[-2]
+        sk_old = self.x[-1] - self.x[-2]
         if strategy=='ODH1':
-            return self.calcAlpha_ODH1(yk_prev, sk_prev)
+            return self.calcAlpha_ODH1(yk_old, sk_old)
         elif strategy=='ODH2':
-            return self.calcAlpha_ODH2(yk_prev, sk_prev)
+            return self.calcAlpha_ODH2(yk_old, sk_old)
         else:
-            alphak1 = self.calcAlpha_ODH1(yk_prev, sk_prev)
-            alphak2 = self.calcAlpha_ODH2(yk_prev, sk_prev)
+            alphak1 = self.calcAlpha_ODH1(yk_old, sk_old)
+            alphak2 = self.calcAlpha_ODH2(yk_old, sk_old)
             if strategy=='adaptive1':
                 if alphak1<=self.kappa*alphak2:
                     return alphak1
@@ -100,22 +103,32 @@ class ODH:
                 else:
                     return alphak2
         
-    def calcAlpha_ODH1(self, yk_prev, sk_prev):
-        Dk_prev = (yk_prev @ yk_prev.transpose()) / (sk_prev.transpose() @ yk_prev)
-        alphak = (self.theta * np.trace(Dk_prev) + sk_prev.transpose() @ yk_prev) / (self.theta + sk_prev.transpose() @ sk_prev)
+    def calcAlpha_ODH1(self, yk_old, sk_old):
+        Dk_old = (yk_old @ yk_old.transpose()) / (sk_old.transpose() @ yk_old)
+        # alphak = (self.theta * np.trace(Dk_old) + sk_old.transpose() @ yk_old) / (self.theta + sk_old.transpose() @ sk_old)
+        alphak = (self.theta * Dk_old + sk_old.transpose() @ yk_old) / (self.theta + sk_old.transpose() @ sk_old)
         return alphak
 
-    def calcAlpha_ODH2(self, yk_prev, sk_prev):
-        Hk_prev = (sk_prev @ sk_prev.transpose()) / (sk_prev.transpose() @ yk_prev)
-        alphak = (self.theta + (yk_prev.transpose() @ yk_prev)) / (self.theta * np.trace(Hk_prev) + sk_prev.transpose() @ yk_prev)
+    def calcAlpha_ODH2(self, yk_old, sk_old):
+        Hk_old = (sk_old @ sk_old.transpose()) / (sk_old.transpose() @ yk_old)
+        # alphak = (self.theta + (yk_old.transpose() @ yk_old)) / (self.theta * np.trace(Hk_old) + sk_old.transpose() @ yk_old)
+        alphak = (self.theta + (yk_old.transpose() @ yk_old)) / (self.theta * Hk_old + sk_old.transpose() @ yk_old)
         return alphak
 
 if __name__=='__main__':
     ## initialize
     n = 5
-    x0 = np.random.rand(n).reshape(n,1)
+    # x0 = 10*np.random.rand(n).reshape(n,1)
+    x0 = 2*np.array([1.3, 0.7, 0.8, 1.9, 1.2])
     objFun = rosen
     gradFun = rosen_der
     A = np.eye(n)
     odhoptim = ODH(x0, objFun, gradFun, A, verbose=True)
     odhoptim.optimize()
+
+    # compare with scipy.minimize
+    print('=================nelder-mead================')
+    res = minimize(rosen, x0, method='nelder-mead',
+               options={'xatol': 1e-8, 'disp': True})
+    
+    print(res.x)
